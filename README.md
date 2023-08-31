290da3a4a1e44fbdf32e952c4e849de211a2e060
jessikamiranda <jeessika.miranda@gmail.com>
2023-08-30 22:07:22 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+<h1 align="center">
+  💻<br>Portfólio - Jessika Miranda
+</h1>
+
+![Resultado final do projeto](assets/image/preview.png)
+
+<h4 align="center"><a href="">Clique para visitar o projeto</a></h4>
+
+## 💼 Tecnologias utilizadas
+
+Foram utilizadas as seguintes tecnologias no desenvolvimento deste site:
+
+- HTML;
+- Sass;
+- JavaScript;
+- ScrollReveal;
+- SVG Inject;
---
+++ assets/js/main.js
@@ -0,0 +1,54 @@
+import { initScrollReveal } from "./scrollReveal.js";
+import { hoverChangeExperience } from "./hoverChangeExperience.js";
+import { typeWrite } from "./typeWrite.js";
+import { hoverChangeDescription } from "./hoverChangeDescription.js";
+import { menu } from "./menu.js";
+
+menu();
+initScrollReveal();
+typeWrite(document.querySelector(".typewriter"));
+
+hoverChangeExperience(
+  ".tcsencon",
+  `Trabalhei como instrutor conteudista em frontend na
+  Digital House, o trabalho consistia em criar conteúdos para as
+  aulas de especialização frontend. Alguns temas dos conteúdos
+  realizados foram: React, Redux, TypeScript, Testes,
+  GraphQL, Next.js, MUI, React Hook Form e styled-components.`,
+  "Professor conteudista em Frontend",
+  "Digital House",
+  "Nov 2021 - Nov 2022 (1 ano)"
+);
+
+hoverChangeDescription(
+  ".html",
+  "HTML é uma linguagem de marcação, onde marcamos os elementos para definir quais informações a página vai exibir."
+);
+hoverChangeDescription(
+  ".css",
+  "CSS é uma linguagem de folha de estilo composta por “camadas”, criado com o propósito de estilizar as páginas."
+);
+hoverChangeDescription(
+  ".js",
+  "JavaScript é uma linguagem de programação que permite a você implementar elementos dinâmicos em páginas web."
+);
+hoverChangeDescription(
+  ".react",
+  "React é uma biblioteca JavaScript com foco em criar interfaces de usuário de forma componetizada."
+);
+hoverChangeDescription(
+  ".next",
+  "Next.js é uma estrutura da web que permite funcionalidades como renderização do lado do servidor e geração de sites estáticos para web baseados em React."
+);
+hoverChangeDescription(
+  ".styled",
+  "styled-components é uma biblioteca que utiliza o conceito de CSS-in-JS, ou seja, que nos permite escrever códigos CSS dentro do Javascript."
+);
+hoverChangeDescription(
+  ".typescript",
+  "TypeScript é um superset do JavaScript que incluir recursos que não estão presentes na linguagem de forma nativa, além de torná-la estática."
+);
+hoverChangeDescription(
+  ".figma",
+  "Figma é uma plataforma colaborativa para construção de design de interfaces e protótipos."
+);
